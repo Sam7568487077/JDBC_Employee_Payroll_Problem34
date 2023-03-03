0395280851663ca619571e64c7f2389cd01aa0f9
Sam K Varghese <ksam071@gmail.com>
2023-03-03 20:27:44 +0530

    Conn. established

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.jdbc_employee_payroll_problem34 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.jdbc_employee_payroll_problem34 to javafx.fxml;
