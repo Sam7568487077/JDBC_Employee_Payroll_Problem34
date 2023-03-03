5a128ee61d512f08e4e12aa3f4081e041855ebf1
Sam K Varghese <ksam071@gmail.com>
2023-03-03 20:18:28 +0530

    First push

---
+++ src/main/java/com/bdlabz/Day34Problem/Main.java
@@ -0,0 +1,7 @@
+package com.bdlabz.Day34Problem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("THis is day 34 Problem");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.jdbc_employee_payroll_problem34 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.jdbc_employee_payroll_problem34 to javafx.fxml;
+    exports com.example.jdbc_employee_payroll_problem34;
+}
